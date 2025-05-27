8f10ec6500c7113259d5f411edf8683fa534d51a
Max <mdolgov322@gmail.com>
2025-05-27 19:42:07 +0400

    fix bug

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
 
 
   const allowedOrigins = [
-    `http://frontend:3000`,
+    `http://95.27.74.242:3000`,
     `http://0.0.0.0:3000`,
     `http://localhost:3000`,
     `http://${DOMAIN}:${PORT}`
